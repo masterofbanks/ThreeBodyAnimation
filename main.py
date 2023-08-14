b98fd293b281eed871e75675f4224931bd5730ab
crackalamoo <harysdalvi@gmail.com>
2023-08-14 00:04:55 -0400

    runge-kutta and simulation setup

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import numpy as np
 from simulation import *
+import matplotlib.pyplot as plt # temporary visualization for testing
+import matplotlib.animation as animation
 # import functions for animation as well
 
 H = 0.01 # step size
 
-x = np.zeros(3) # three bodies
-y = np.zeros(3)
-vx = np.zeros(3)
-vy = np.zeros(3)
+x = np.zeros((3,2)) # three bodies, two components: position and velocity
+y = np.zeros((3,2))
+m = np.ones(3) # masses of the three bodies
+
+# set initial conditions
+x[0] = np.array([0,0])
+y[0] = np.array([0,0])
+m *= 1e5
+m[0] = 1e10
+for i in range(1,3):
+    x[i] = np.random.normal(size=(2,))
+    y[i] = np.random.normal(size=(2,))
+    dist = np.linalg.norm([x[i]-x[0], y[i]-y[0]])
+    vi = np.sqrt(G*m[0]/dist)
+    x[i][1] = vi*(y[i][0]-y[0][0])/dist
+    y[i][1] = -vi*(x[i][0]-x[0][0])/dist
+
+SIM_LEN = 500
+simulation = []
+for i in range(SIM_LEN):
+    x, y = simulate_step(x, y, m, H)
+    simulation.append([np.copy(x[:,0]), np.copy(y[:,0])])
+
+max_coord = np.max(np.abs(np.concatenate(simulation)))
+fig = plt.figure()
+scatter = plt.scatter(x[:,0], y[:,0])
+ani = animation.FuncAnimation(fig, lambda i: scatter.set_offsets(np.array([simulation[i][0], simulation[i][1]]).T),
+                              frames=range(SIM_LEN), interval=20)
+fig.get_axes()[0].set_xlim(-max_coord, max_coord)
+fig.get_axes()[0].set_ylim(-max_coord, max_coord)
+
+plt.show()
--- simulation.py
+++ simulation.py
@@ -1,5 +1,31 @@
 import numpy as np
+EPS = 1e-7 # epsilon for numerical stability
 
 def runge_kutta_step(f, y, h):
     # assuming f does not depend on t (time symmetry)
-    pass
+    k1 = f(y)
+    k2 = f(y + h/2*k1)
+    k3 = f(y + h/2*k2)
+    k4 = f(y + h*k3)
+    return y + h/6*(k1 + 2*k2 + 2*k3 + k4)
+
+G = 6.67408e-11 # gravitational constant
+def gravity(x1, x2, y1, y2, m2):
+    # acceleration due to gravity of m2 on m1
+    return -G*m2*(x1-x2)/(np.linalg.norm((x1-x2, y1-y2))+EPS)**3
+
+def simulate_step(x, y, m, h):
+    # x, y are each arrays of shape (N,2) for N bodies and two components:
+    # one position, one velocity.
+    # m is an array of shape (N) for the masses of N bodies
+    N = x.shape[0]
+    new_x = np.copy(x)
+    new_y = np.copy(y)
+    for i in range(N):
+        for j in range(N):
+            if i == j: continue
+            f_x = lambda xi: np.array([xi[1], gravity(xi[0], x[j,0], y[i,0], y[j,0], m[j])])
+            f_y = lambda yi: np.array([yi[1], gravity(yi[0], y[j,0], x[i,0], x[j,0], m[j])])
+            new_x[i] = runge_kutta_step(f_x, new_x[i], h)
+            new_y[i] = runge_kutta_step(f_y, new_y[i], h)
+    return new_x, new_y
